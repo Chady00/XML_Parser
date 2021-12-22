98b485397544c145627584c5db68e54404d1362a
Chady00 <84717550+Chady00@users.noreply.github.com>
2021-12-23 00:26:38 +0200

    Update format.h

--- Old_codes/headers/format.h
+++ Old_codes/headers/format.h
@@ -1,1 +1,28 @@
-dd
+/*============================================================================
+// Name        : xml_format.h
+// Author      :
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================*/
+#ifndef XML_FORMAT_H_
+#define XML_FORMAT_H_
+
+/**************************** HEADER *****************************/
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include<vector>
+#include <stack>
+#include<string>
+using namespace std;
+
+
+/********************** FUNCTION PROTOTYPES *********************/
+void print_vect(vector<string> vect);
+void adjust_ident(string *ptr,int a,int data_start);
+int get_start(string *line);
+void format();
+
+
+#endif /* MINIFY_FUNCTION_H_ */
