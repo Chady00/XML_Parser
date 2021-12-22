b39cdb8e18c8b695dcff602abddb7324cb7b609e
Chady00 <84717550+Chady00@users.noreply.github.com>
2021-12-23 00:47:35 +0200

    Update format.h

--- GUI/GUI_Headers/format.h
+++ GUI/GUI_Headers/format.h
@@ -1,1 +1,9 @@
+#ifndef FORMAT_H
+#define FORMAT_H
+#include"mainwindow.h"
+/*********************** HEADERS ****************************/
+void format();
+void adjust_ident(string *ptr,int a,int data_start);
+int get_start(string *line);
+#endif // FORMAT_H
 
