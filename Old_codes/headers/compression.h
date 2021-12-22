cbdef042dc8126366037f976eb0737d8565cd156
shawky20 <67594814+shawky20@users.noreply.github.com>
2021-12-23 01:03:15 +0200

    Update compression.h

--- Old_codes/headers/compression.h
+++ Old_codes/headers/compression.h
@@ -1,41 +1,53 @@
 #ifndef CMP_FUNCTION_H_
 #define CMP_FUNCTION_H_
 
-/********************** Headers *********************/
-//#include <bits/stdc++.h>
-#include <iostream> 
-#include <string>
-#include <queue>
+/******** Headers *******/
+#include<string>
+#include<string.h>
+#include<queue>
+#include<iostream>
 #include <fstream>
 #include <vector>
 using namespace std;
 
+extern string in_s;//this is an extern variable
 
+/******** Classes *******/
 
-/********************** Classes *********************/
-
-class node
+class Node
 {
 public:
     int freq;
-    char data = ' ';
-    string code; // the output code for each node
-    node *left;
-    node *right;
+    char data;
+    string code; // the output code for each Node
+    Node *left;
+    Node *right;
 
 public:
-    node(int freq, char data, node *left, node *right);
-    node(int freq, node *left, node *right);
+    Node(int freq, char data, Node *left, Node *right);
+    Node(int freq, Node *left, Node *right);
 };
 
 class Compare // to be passed as the third parameter of priorety queue
 {
 public:
-    /*impelemnting the comprator*/
-    bool operator()(node *a, node *b /*the top */) // to make it min heap 
+    bool operator()(Node *a, Node *b)
     {
         return (a->freq > b->freq); // least freq at the top to take large code
-    } // if true put it as a node else it will sift up and be the new top 
+    }
 };
+
+/******** Function Prototypes *******/
+void compress();
+void decompress();
+void save_to_file(void);
+void seperate_codes(void);
+void get_out_string(void);
+void add_zeros(void);
+void get_code(void);
+void traverse(Node *ptr, string str);
+void create_tree(void);
+void storein_q(void);
+void get_freqs();
 
 #endif /*CMP_FUNCTION_H_ */
