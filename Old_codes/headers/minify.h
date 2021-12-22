da5020724a9478733566a014426cec1e07ad7d01
sherif14 <85257402+sherif14@users.noreply.github.com>
2021-12-23 00:19:43 +0200

    Update minify.h

--- Old_codes/headers/minify.h
+++ Old_codes/headers/minify.h
@@ -1,1 +1,23 @@
+/*
+ * minify_function.h
+ *
+ *  Created on: Dec 9, 2021
+ *      Author: LENOVO
+ */
 
+#ifndef MINIFY_FUNCTION_H_
+#define MINIFY_FUNCTION_H_
+
+/********************** Headers *********************/
+#include <string> 
+#include <iostream> 
+#include <assert.h> 
+#include <fstream>
+
+
+using namespace std;
+
+/********************** Function Prototypes *********************/
+void minify(string file_name);
+
+#endif /* MINIFY_FUNCTION_H_ */
