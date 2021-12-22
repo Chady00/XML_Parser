3184912a435b94ee54b9232ec20c86eee647a4f6
omarosamaa <84813775+omarosamaa@users.noreply.github.com>
2021-12-23 00:05:07 +0200

    Create xml_to_json.h

---
+++ Old_codes/headers/xml_to_json.h
@@ -0,0 +1,33 @@
+#ifndef trees
+#define trees
+#include <iostream>
+#include <vector>
+using namespace std;
+class node{
+    public:
+    string tagname;
+    vector<node*>childern;
+    string tagdata;
+    node* parent;
+
+    public:
+    node(void);
+    node(string name);
+    node(string name,string data);
+    friend class tree;
+};
+class tree{
+    node*root;
+    public:
+    tree();
+    node*get_root(void);
+    vector<node*>get_children(node*n);
+    void add_root(node*r);
+    string get_tagdata(node*r);
+    string get_name(node*r);
+    node*add_node(string name);
+    void add_data(node*n,string data);
+    void add_child(node*parent,node*child);
+
+};
+#endif
