3ae9d55ff0e1b7e87d084a073175b55706cbc89f
shehab <shehabthelegand@gmail.com>
2022-02-08 03:48:46 +0200

    Update Graph_representation.cpp

--- GUI/GUI Source_codes/Graph_representation.cpp
+++ GUI/GUI Source_codes/Graph_representation.cpp
@@ -1,1 +1,202 @@
 
+
+
+
+
+
+
+
+string get_name(string str_in, int start_ind)
+{
+    string name = "";
+    string open = "<name>";
+    string close = "</name>";
+    // int opening = in_str.find(tag, start_index);
+
+    std::size_t found_open = str_in.find(open, start_ind) + 6;
+    std::size_t found_close = str_in.find(close, start_ind);
+    if (found_open != std::string::npos && found_close != std::string::npos)
+    {
+
+        // std::cout << "first 'name' found at: " << found_open << '\n';
+        // std::cout << "first 'close name' found at: " << found_close << '\n';
+        // std::cout << str_in.substr(found_open, found_close - found_open) << '\n';
+        name = str_in.substr(found_open, found_close - found_open);
+    }
+
+    return name;
+}
+
+
+
+
+
+int get_followers(string str_in, int start_ind, G_LinkedList *user_list)
+{
+    int start_of_tag = 0;
+    int end_of_tag = 0;
+    int start_of_prev = 0;
+    int end_of_prev = 0;
+    string tag = "";
+    string data_between = "";
+    int index_in = start_ind;
+    string curr_char;
+    int len_in = str_in.length();
+    string out_str = "";
+    bool get_data = true;
+    int end_of_user = 0;
+    string follower_id = "";
+    string follower_name="";
+
+    while (get_data)
+    {
+        // cout << str_in << endl;
+        curr_char = str_in[index_in];
+        if (curr_char == "<")
+        {
+            start_of_prev = start_of_tag;
+            end_of_prev = end_of_tag;
+            start_of_tag = index_in;
+            end_of_tag = str_in.find(">", start_of_tag);
+
+            tag = str_in.substr(start_of_tag, end_of_tag + 1 - start_of_tag);
+            if (tag == "</user>")
+            {
+                end_of_user = end_of_tag + 1;
+                return end_of_user;
+            }
+            if (tag == "</id>")
+            {
+                follower_id = (index_in != 0) ? str_in.substr(end_of_prev + 1, start_of_tag - end_of_prev - 1) : "";
+                // cout << "follower_id >>" << follower_id << endl;
+                follower_name = get_name(str_in, end_of_tag);
+                user_list->push_front(int(stoi(follower_id)), follower_name);
+            }
+
+            index_in = end_of_tag;
+        }
+        index_in++;
+
+        if (index_in == len_in)
+        {
+
+            get_data = false;
+        }
+    }
+    return index_in;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+vector<G_LinkedList *> fill_graph_data(string xml_data)
+{
+    vector<G_LinkedList *> users_vector;
+
+    int start_of_tag = 0;
+    int end_of_tag = 0;
+    int start_of_prev = 0;
+    int end_of_prev = 0;
+    string tag = "";
+    string data_between = "";
+    int index_in = 0;
+    string curr_char;
+    int len_in = xml_data.length();
+    string out_str = "";
+    bool get_data = true;
+    int end_of_user = 0;
+    string user_id = "";
+    string user_name="";
+    // int user_count = 0;
+
+    while (get_data)
+    {
+        curr_char = xml_data[index_in];
+        if (curr_char == "<")
+        {
+            start_of_prev = start_of_tag;
+            end_of_prev = end_of_tag;
+            start_of_tag = index_in;
+            end_of_tag = xml_data.find(">", start_of_tag);
+
+            tag = xml_data.substr(start_of_tag, end_of_tag + 1 - start_of_tag);
+            if (tag == "</id>")
+            {
+                user_id = (index_in != 0) ? xml_data.substr(end_of_prev + 1, start_of_tag - end_of_prev - 1) : "";
+                user_name = get_name(xml_data, index_in);
+                // cout << "user_id >>" << user_id << endl;
+
+                G_LinkedList *user_list = new G_LinkedList();
+                users_vector.push_back(user_list);
+                // // push the header in a vector
+
+                end_of_user = get_followers(xml_data, end_of_tag + 1, user_list);
+                user_list->push_front(int(stoi(user_id)), user_name);
+                end_of_tag = end_of_user;
+            }
+            index_in = end_of_tag;
+        }
+        index_in++;
+
+        if (index_in == len_in)
+        {
+
+            get_data = false;
+        }
+    }
+    return users_vector;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
